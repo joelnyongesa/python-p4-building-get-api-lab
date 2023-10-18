4512c40d841b355cf38f6a4984495d8eb0814a6c
joelnyongesa <joelnyongesa148@gmail.com>
2023-10-18 14:13:59 +0300

    Completed assignment

--- server/app.py
+++ server/app.py
@@ -19,20 +19,51 @@ def index():
     return '<h1>Bakery GET API</h1>'
 
 @app.route('/bakeries')
+# GET /bakeries: returns an array of JSON objects for all bakeries in the database.
 def bakeries():
-    return ''
+    bakeries_list = [bakery.to_dict() for bakery in Bakery.query.all()]
+
+    response = make_response(
+        jsonify(bakeries_list),
+        200
+    )
+    
+    return response
 
 @app.route('/bakeries/<int:id>')
+# GET /bakeries/<int:id>: returns a single bakery as JSON with its baked goods nested in an array. Use the id from the URL to look up the correct bakery.
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.filter_by(id=id).first().to_dict()
+
+    response = make_response(
+        jsonify(bakery),
+        200
+    )    
+    return response
 
 @app.route('/baked_goods/by_price')
+# GET /baked_goods/by_price: returns an array of baked goods as JSON, sorted by price in descending order. (HINT: how can you use SQLAlchemy to sort the baked goods in a particular order?)
 def baked_goods_by_price():
-    return ''
+    baked_goods = [baked_good.to_dict() for baked_good in BakedGood.query.order_by(db.desc('price')).all()]
+    
+    response = make_response(
+        jsonify(baked_goods),
+        200
+    )
+
+    return response
 
 @app.route('/baked_goods/most_expensive')
+# GET /baked_goods/most_expensive: returns the single most expensive baked good as JSON. (HINT: how can you use SQLAlchemy to sort the baked goods in a particular order and limit the number of results?)
 def most_expensive_baked_good():
-    return ''
+    most_expensive = BakedGood.query.order_by(db.desc('price')).first().to_dict()
+
+    response = make_response(
+        jsonify(most_expensive),
+        200
+    )
+
+    return response
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
